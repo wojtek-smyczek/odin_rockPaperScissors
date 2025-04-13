2f81c65d6033aaddb618224a4080efefe7069de8
wojtek-smyczek <wojciech.smyczek@student.uj.edu.pl>
2025-04-13 21:28:23 +0200

    Finished game RPS

--- script.js
+++ script.js
@@ -10,6 +10,9 @@
 
 // console.log(getComputerChoice())
 
+let humanChoice;
+let computerChoice;
+
 
 let rock = "rock";
 let paper = "paper";
@@ -24,16 +27,69 @@ function getComputerChoice() {
     let number = getRandom(3);
 
     if (number === 1) {
-        return rock;
+        return "rock";
     } else if (number === 2) {
-        return paper;
+        return "paper";
     } else {
-        return scissors;
+        return "scissors";
     }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Let's play sccissors paper rock! Pick one.");
+    humanChoice.toLowerCase();
+    return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const map1 = new Map();
+
+map1.set("paper", "rock");
+map1.set("scissors", "paper");
+map1.set("rock", "scissors");
+
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice === humanChoice) {
+        return "Draw";
+    } else if (map1.get(humanChoice) === computerChoice) {
+        humanScore++;
+        return "You won!";
+    } else {
+        computerScore++;
+        return "You lost";
+    }
+}
+
+function playGame() {
+    let num = prompt("How many rounds you wanna play?");
+
+    for (let round = 0; round < num; round++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection)
+        // console.log(playRound(humanSelection, computerSelection));
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You won the game.");
+    } else if (computerScore > humanScore) {
+        console.log("You lost");
+    } else {
+        console.log("Its a tie!");
+    }
+
 
 }
 
-console.log(getComputerChoice());
+playGame();
+
+
+
+
+
+
 
 
 
